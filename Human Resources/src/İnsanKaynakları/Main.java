9eb5d8c01f1eb4593ee65b6270cff80680c32511
Furkan <89587238+furkaansolmaz@users.noreply.github.com>
2022-05-19 11:37:42 +0300

    Deneme Yorum Satırı Silinsi

--- Human Resources/src/İnsanKaynakları/Main.java
+++ Human Resources/src/İnsanKaynakları/Main.java
@@ -5,6 +5,6 @@ public class Main {
         public static void main(String[] args) {
             HumanManager insank = new HumanManager();
             insank.menu();
-            //Deneme
+
         }
 }
